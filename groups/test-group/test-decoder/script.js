87c520853cde84ca9fadab644a68a22f43b9693a
Arjan van IJzendoorn <arjan.van.ijzendoorn@blockbax.com>
2023-08-08 17:21:36 +0200

    Remove test group and decoder

--- groups/test-group/test-decoder/script.js
+++
@@ -1,8 +0,0 @@
-// Update template to version 2
-function convertPayload(payload, context) {
-    context.addMeasurement(
-        "test-ingestion-id2",
-        payload.number,
-        date(payload.date)
-    );
-}
