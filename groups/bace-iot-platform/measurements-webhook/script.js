f6b40e2c116916aac8fdfe05ea89a515bd56bf5c
Arian Maghsoudnia <arian.m95@gmail.com>
2023-11-02 17:56:21 +0100

    Improve payload converter and tests

--- groups/bace-iot-platform/measurements-webhook/script.js
+++ groups/bace-iot-platform/measurements-webhook/script.js
@@ -2,6 +2,10 @@ function convertPayload(payload, context) {
     payload.forEach((item) => {
         const { label, relations, value, timestamp } = item;
 
+        if (!label || !relations["source_device"] || value === undefined || !timestamp) {
+            // Skip processing this item if any of the required properties is undefined
+            return; // Continue to the next iteration
+        }
         // source_device is the subject external id and label is the metric external id
         const source_device = relations["source_device"];
         const ingestionId = `${source_device}$${toKebabCase(label)}`;
@@ -15,9 +19,6 @@ function convertPayload(payload, context) {
 }
 
 function toKebabCase(inputString) {
-    if (inputString === undefined) {
-        return "undefined";
-    }
     return inputString
         .trim() // Remove leading and trailing spaces
         .replace(/\s+/g, "-") // Replace spaces with hyphens
@@ -26,6 +27,5 @@ function toKebabCase(inputString) {
         .replace(/([a-zA-Z]+)(\d+)/g, "$1-$2") // Handle numbers after textual parts
         .replace(/([a-z])([A-Z])/g, "$1-$2") // Insert hyphens between camel case words
         .replace(/([A-Z]+)([A-Z][a-z])/g, "$1-$2") // Handle consecutive uppercase letters
-        .replace(/[\s_]+/g, "-") // Replace spaces or underscores with hyphens
         .toLowerCase(); // Convert to lowercase
 }
