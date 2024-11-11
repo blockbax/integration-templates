e63417f7a6e203c9476973beb2daaf4a4aabbc42
Arian Maghsoudnia <arian.maghsoudnia@blockbax.com>
2024-11-11 11:16:24 +0100

    Fix unconventional variable naming

--- groups/bace-iot-platform/events-webhook/script.js
+++ groups/bace-iot-platform/events-webhook/script.js
@@ -8,8 +8,8 @@ function convertPayload(payload, context) {
 
         const ingestionIdEventTypeLabel = `${source_device}$event-type-label`;
         const ingestionIdEventValueDescription = `${source_device}$event-value-description`;
-        const IngestionDate = date(occurred_at * 1000);
-        context.addMeasurement(ingestionIdEventTypeLabel, event_type_label, IngestionDate);
-        context.addMeasurement(ingestionIdEventValueDescription, event_value_description, IngestionDate);
+        const ingestionDate = date(occurred_at * 1000);
+        context.addMeasurement(ingestionIdEventTypeLabel, event_type_label, ingestionDate);
+        context.addMeasurement(ingestionIdEventValueDescription, event_value_description, ingestionDate);
     });
 };
